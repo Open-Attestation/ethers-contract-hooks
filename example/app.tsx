b8d31b326f2b083f3fd87a8a62d54710c0b500ce
Raymond Yeh <ray@geek.sg>
2020-03-25 16:37:12 +0800

    wip

--- example/app.tsx
+++ example/app.tsx
@@ -1,16 +1,9 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import { HelloWorld } from "../src";
+import { TestHook } from "../src";
 
 const App = (): React.ReactElement => {
-  const [name, setName] = useState("");
-  return (
-    <div>
-      <h1>Enter your name</h1>
-      <input value={name} onChange={e => setName(e.target.value)} />
-      <HelloWorld name={name} />
-    </div>
-  );
+  return <TestHook />;
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState, useEffect } from "react";
+import { ContractTransaction, providers, Wallet, Contract, Signer, ContractFunction } from "ethers";
+import { connect } from "./oa";
+import { DocumentStore } from "./contracts/DocumentStore";
 
 interface HelloWorld {
   name: string;
@@ -6,4 +9,47 @@ interface HelloWorld {
 
 export const HelloWorld: FunctionComponent<HelloWorld> = ({ name }) => {
   return <div data-testid="hello-world">Hello {name}</div>;
+};
+
+type ContractFunctionState = "uninitialized" | "pending" | "received" | "error" | "completed";
+
+function useContractFunctionHook<T extends Contract>(contract: Promise<T>, method: string) {
+  const [state, setState] = useState<ContractFunctionState>("uninitialized");
+  const transact = async (...params: any[]) => {
+    const contractMethod = (await contract).functions[method];
+    const deferredTx = contractMethod(...params);
+    setState("pending");
+    try {
+      const receipt = await deferredTx;
+      console.log("receipt", receipt);
+      setState("received");
+      const results = await receipt.wait();
+      console.log("results", results);
+      setState("completed");
+    } catch (e) {
+      setState("error");
+    }
+  } as (await contract).functions[method];
+  return { state, transact };
+}
+
+const provider = new providers.JsonRpcProvider();
+const signer = new Wallet("cd1452521e0bdbe560466fcc9e43fbebc9667d5fa7f5112cad7754553c6f7567", provider);
+
+export const TestHook: FunctionComponent = () => {
+  const contract = connect(
+    "0xD3C951dBE13CF63E112c42dF8a92d0faB7eC219c",
+    signer
+  );
+  const { state, transact } = useContractFunctionHook(contract, "issue");
+  const handleTransaction = () => {
+    transact("0xd6df43b7372d4c696612f5ca454489f40e76ab6b0575110a9965f083fafd2dd9");
+  };
+
+  return (
+    <div data-testid="hello-world">
+      <button onClick={handleTransaction}>Issue Transaction</button>
+      {state}
+    </div>
+  );
 };
